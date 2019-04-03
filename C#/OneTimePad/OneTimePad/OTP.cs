1df049654e2ef5e9c974dd036a9b009344302f91
ilpadre <ilpadre1953@gmail.com>
2019-04-03 17:58:10 -0400

    Added gitignore

---
+++ C#/OneTimePad/OneTimePad/OTP.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OneTimePad
+{
+    class OTP
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
