1ec99270a00993bc19f2f8932fd63643a14e7bc2
Jim Kriz <jim@kriz.net>
2019-04-19 15:09:02 -0400

    Implement the library functions

--- C#/OneTimePad/OneTimePad.Lib/OTP.cs
+++ C#/OneTimePad/OneTimePad.Lib/OTP.cs
@@ -9,17 +9,40 @@ namespace OneTimePad.Lib
         private static Random random = new Random();
         public string Generate(int keyLengthInBytes)
         {
-            return "Not a random string";
+            // Borrowed this code from: https://gist.github.com/khalilovcmd/494988c43b6c205f9d76
+            // The important part here is that the key generated has to be a random set of *ASCII characters*, 
+            // not just any old bytes (that's why the random.NextBytes() was not working)
+            int asciiCharacterStart = 65; // from which ascii character code the generation should start
+            int asciiCharacterEnd = 122; // to which ascii character code the generation should end
+            var result = new Char[keyLengthInBytes];
+            for(int i = 0; i < keyLengthInBytes; i++)
+            {
+                result[i] = (char)(random.Next(asciiCharacterStart, asciiCharacterEnd + 1) % 255);
+            }
+
+            return new string(result);
         }
 
         public string Encrypt(string key, string message)
         {
-            return "Hardly encrypted";
+            return ExclusiveOr(key, message);
         }
 
         public string Decrypt(string key, string cipherText)
         {
-            return "Wonder what the original message was?";
+            return ExclusiveOr(key, cipherText);
+        }
+
+        private string ExclusiveOr(string s1, string s2)
+        {
+            var s1Chars = s1.ToCharArray();
+            var s2Chars = s2.ToCharArray();
+            var result = new Char[s1Chars.Length];
+            for (var i = 0; i < s1Chars.Length; i++)
+            {
+                result[i] = (char)(s2Chars[i] ^ s1Chars[i]);
+            }
+            return new string(result);
         }
     }
 }
